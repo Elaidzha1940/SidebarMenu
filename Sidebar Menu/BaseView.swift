4045e1b38aa97ce6c37b0374412ae3755c98d6e6
Elaidzha1940 <eli080293@gmail.com>
2023-05-31 19:24:49 +0700

    Sidebar Menu + added a new File / BaseView

---
+++ Sidebar Menu/BaseView.swift
@@ -0,0 +1,25 @@
+//  /*
+//
+//  Project: Sidebar Menu
+//  File: BaseView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 31.05.2023
+//
+//  */
+
+import SwiftUI
+
+struct BaseView: View {
+    var body: some View {
+        
+        Text("World!")
+            .font(.system(.largeTitle))
+            .bold()
+    }
+}
+
+struct BaseView_Previews: PreviewProvider {
+    static var previews: some View {
+        BaseView()
+    }
+}
