3babd69d8e32145d6f8dba5c5e2204eb4414e610
Elaidzha1940 <eli080293@gmail.com>
2023-05-31 18:42:12 +0700

    Sidebar Menu

---
+++ Sidebar Menu/Sidebar_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sidebar_MenuApp.swift
+//  Sidebar Menu
+//
+//  Created by Elaidzha Shchukin on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Sidebar_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
