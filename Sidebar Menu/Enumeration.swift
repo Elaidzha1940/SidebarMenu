d6f7750d2cba3d278a0de47e752130721d1b6697
Elaidzha1940 <eli080293@gmail.com>
2023-05-31 19:21:45 +0700

    Sidebar Menu + added a new File / Enumeration

--- Sidebar Menu/Enumeration.swift
+++ Sidebar Menu/Enumeration.swift
@@ -11,4 +11,47 @@ import SwiftUI
 
 enum EnumerationItem {
     
+    case dashboard
+    case clients
+    case deals
+    case calendar
+    case reports
+    case profile
+   
+    var title: String {
+        
+        switch self {
+        case .dashboard:
+            return "Dashboard"
+        case .clients:
+            return "Clients"
+        case .deals:
+            return "Deals"
+        case .calendar:
+            return "Calendar"
+        case .reports:
+            return "Reports"
+        case .profile:
+            return "Profile"
+        }
+    }
+    
+    var icon: some View {
+        
+        switch self {
+        case .dashboard:
+            return Image(systemName: "timer")
+        case .clients:
+            return Image(systemName: "person.badge.plus.fill")
+        case .deals:
+            return Image(systemName: "car.fill")
+        case .calendar:
+            return Image(systemName: "calendar")
+        case .reports:
+            return Image(systemName: "chart.bar.fill")
+        case .profile:
+            return Image(systemName: "person.fill")
+        }
+    }
 }
+
